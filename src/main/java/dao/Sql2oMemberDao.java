c56c370befa95a55159a5a8d80fe935c87cacf59
Rich Garrick <richg341@gmail.com>
2017-08-18 12:52:31 -0700

    pass test to delete member by id

--- src/main/java/dao/Sql2oMemberDao.java
+++ src/main/java/dao/Sql2oMemberDao.java
@@ -54,6 +54,17 @@ public class Sql2oMemberDao {
         }
     }
 
+    public void deleteById(int id) {
+        String sql = "DELETE from member WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
 
 
 }
